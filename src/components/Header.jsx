6c8ff8d8c0d89fbb1ef41bd1e2ede4a485bbefe6
Sergey Demidov <ssevod@gmail.com>
2020-08-17 21:19:01 +0300

    1

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className = 'header'>
+        <img src = 'https://upload.wikimedia.org/wikipedia/ru/b/b7/Discord_logo_svg.svg'/>
+      </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className = 'content'>
+        <div>
+          <img src ='https://gazeta.spb.ru/wp-content/uploads/2019/06/beach-2179624_960_720.jpg' />
+        </div>
+        <div>
+          ava + dis
+        </div>
+        <div>
+          my post
+          <div>
+            new post
+          </div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 1
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Profile;
