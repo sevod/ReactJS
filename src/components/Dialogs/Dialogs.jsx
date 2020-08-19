f7f670ba7860be38df6405413b82766a137695a2
Sergey Demidov <ssevod@gmail.com>
2020-08-19 12:00:53 +0300

    18

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import s from './Dialogs.module.css'
+
+const Dialogs = (props) => {
+    return (
+        <div >
+            Dilogs
+        </div>
+    );
+}
+
+export default Dialogs;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,17 +3,17 @@ import s from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
-  return (
-    <div className={s.content}>
-      <div>
-        <img src='https://gazeta.spb.ru/wp-content/uploads/2019/06/beach-2179624_960_720.jpg' />
-      </div>
-      <div>
-        ava + dis
+    return (
+        <div>
+            <div>
+                <img src='https://gazeta.spb.ru/wp-content/uploads/2019/06/beach-2179624_960_720.jpg'/>
+            </div>
+            <div>
+                ava + dis
+            </div>
+            <MyPosts/>
         </div>
-      <MyPosts />
-    </div>
-  );
+    );
 }
 
 export default Profile;
