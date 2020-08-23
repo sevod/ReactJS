aeffe6fb8da9b3002dab805b1e91d270cb2e1b50
Sergey Demidov <ssevod@gmail.com>
2020-08-23 15:33:43 +0300

    31

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -8,15 +8,22 @@ const MyPosts = (props) => {
         postData => (<Post message={postData.message} likesCount={postData.likesCount}/>)
     );
 
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        alert(text);
+    }
+
     return (
         <div className={s.postBlock}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostElement}></textarea>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={ addPost }>Add post</button>
                 </div>
             </div>
             <div className={s.posts}>
