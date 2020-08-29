bd813a6637cd88205b9cbf014df3fc941c0447ec
Sergey Demidov <ssevod@gmail.com>
2020-08-29 22:52:41 +0300

    52

--- README.md
+++ README.md
@@ -424,3 +424,7 @@ axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response =>
             debugger;
         });
 ```
+
+**Урок 52**
+
+Side Effect
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,17 +5,21 @@ import userPhoto from '../../../src/assets/images/user.png'
 
 
 const Users = (props) => {
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items);
-        });
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items);
+            });
+        }
     }
     return <div>
+        <button onClick={getUsers}>Get Users</button>
         {
             props.users.map(users => <div key={users.id}>
                 <span>
                     <div>
-                        <img src={users.photos.small != null ? users.photos.small : userPhoto} className={styles.userPhoto}/>
+                        <img src={users.photos.small != null ? users.photos.small : userPhoto}
+                             className={styles.userPhoto}/>
                     </div>
                     <div>
                         {users.followed ?
