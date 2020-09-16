fee6073ffe9a1589b4db25d3c6d2b7369f86cf44
Sergey Demidov <ssevod@gmail.com>
2020-09-16 15:40:41 +0300

    Updates

---
+++ precache-manifest.200bc78caa0b6aa165f6e9b4211b7eb6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f6a828a39558065b956b00389188eec",
+    "url": "/ReactJS/index.html"
+  },
+  {
+    "revision": "22b4c2f28e6b00499cc6",
+    "url": "/ReactJS/static/css/3.bfb86e6f.chunk.css"
+  },
+  {
+    "revision": "f14e45d9395ceab0128e",
+    "url": "/ReactJS/static/css/4.3d65222f.chunk.css"
+  },
+  {
+    "revision": "2f3adbb94c8a50b07d61",
+    "url": "/ReactJS/static/css/5.5e872ec0.chunk.css"
+  },
+  {
+    "revision": "eb5f4e20760d8b7af1ed",
+    "url": "/ReactJS/static/css/main.fbccf373.chunk.css"
+  },
+  {
+    "revision": "bf04ec670aca615cd77f",
+    "url": "/ReactJS/static/js/2.1ef12679.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactJS/static/js/2.1ef12679.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22b4c2f28e6b00499cc6",
+    "url": "/ReactJS/static/js/3.d73ff471.chunk.js"
+  },
+  {
+    "revision": "f14e45d9395ceab0128e",
+    "url": "/ReactJS/static/js/4.7152e76c.chunk.js"
+  },
+  {
+    "revision": "2f3adbb94c8a50b07d61",
+    "url": "/ReactJS/static/js/5.ad5c2d50.chunk.js"
+  },
+  {
+    "revision": "eb5f4e20760d8b7af1ed",
+    "url": "/ReactJS/static/js/main.0347e455.chunk.js"
+  },
+  {
+    "revision": "c4ba2bb68a0ccc825ddc",
+    "url": "/ReactJS/static/js/runtime-main.03493e63.js"
+  },
+  {
+    "revision": "fea7bf8faf8bf54218b88fea2ea8c62c",
+    "url": "/ReactJS/static/media/preloader.fea7bf8f.svg"
+  },
+  {
+    "revision": "498d600c434a3808564afa34f09b3d93",
+    "url": "/ReactJS/static/media/user.498d600c.png"
+  }
+]);
